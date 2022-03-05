4307304a74179e9c70eb124ae8e8503e3d49824a
Jonas Galvez <jonasgalvez@gmail.com>
2022-03-05 01:52:27 -0300

    add .eslintrc

---
+++ packages/fastify-vite/html.js
@@ -0,0 +1,20 @@
+const unescapedBacktick = /(?<!\\)`/g
+
+function compileIndexHtml (source) {
+  const indexHtml = (
+    '(function (req, fragments) {\n' +
+    `  return \`${
+      source
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('<html>', '<html${fragments.html ? fragments.html : \'\'}>')
+        // eslint-disable-next-line no-template-curly-in-string
+        .replace('<body>', '<body${fragments.body ? fragments.body : \'\'}>')
+        .replace(unescapedBacktick, '\\`')
+    }\`\n` +
+    '})'
+  )
+  // eslint-disable-next-line no-eval
+  return (0, eval)(indexHtml)
+}
+
+module.exports = { compileIndexHtml }
