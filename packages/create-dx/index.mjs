7bce0ef17ef35f5af9eb79a8ee3a1315d2326160
David Meir-Levy <davidmeirlevy@gmail.com>
2022-04-18 11:27:23 +0300

    Implement create-dx package

---
+++ packages/create-dx/index.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+
+const [ , , ...argv ] = process.argv;
+
+execSync('npx fastify-dx setup ' + argv.join(' '), { stdio: 'inherit' });
