2e1316cf37b331145256f7ad9b5e57dd936dab45
Jonas Galvez <jonasgalvez@gmail.com>
2022-06-30 15:14:41 -0300

    feat(svelte): allow state changes to be serialized into hydration payload

--- starters/svelte/client/root.svelte
+++ starters/svelte/client/root.svelte
@@ -3,22 +3,25 @@ import 'uno.css'
 import { proxy } from 'sveltio'
 import { Router, Route } from 'svelte-routing'
 import DXRoute from '/dx:route.svelte'
+import { isServer } from '/dx:core.js'
 
 export let url = null
 export let payload
 
-let state = proxy(payload.serverRoute.state)
+let state = isServer
+  ? payload.serverRoute.state
+  : proxy(payload.serverRoute.state)
 </script>
 
 <Router url="{url}">
   {#each payload.routes as { path, component }}
     <Route path="{path}" let:location>
-    	<DXRoute 
+      <DXRoute 
         path={path}
-    	  location={location}
-    		state={state}
+        location={location}
+        state={state}
         payload={payload}
-    		component={component} />
+        component={component} />
     </Route>
   {/each}
 </Router>
